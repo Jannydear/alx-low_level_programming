a4451da90250236c8c88d2c4c909c49eb5e6042e
Jannydear <jannydear112@gmail.com>
2023-04-22 14:56:14 -0700

    b

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,20 @@ int main(void)
 {
 	int i, k = '0', j;
 
-	for(i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for(j = k; j <= '9'; j++)
+		for (j = k; j <= '9'; j++)
 		{
-			if(i == j)
+			if (i == j)
 			{
 				putchar(i);
 				putchar(j);
 			}
-			if(i == j)
+			if (i == j)
 			{
 				continue;
 			}
-			if(i == '8' && j == '9')
+			if (i == '8' && j == '9')
 			{
 				break;
 			}

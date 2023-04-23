a1de74a84406829054225772d849e03553f74a24
Jannydear <jannydear112@gmail.com>
2023-04-22 23:44:04 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; < 99; i++)
+	for (i = 0; '<' 99; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
 			putchar('0' + i / 10);
 			putchar('0' = i % 10);
 
-			putchar(' ')
-			putchar('0' + j/ 10);
+			putchar(' ');
+			putchar('0' + j / 10);
 			putchar('0' + j % 10);
 
 			if (i == 98 && j == 99)
-				brreak;
+				break;
 			putchar(',');
 			putchar(' ');
 		}
